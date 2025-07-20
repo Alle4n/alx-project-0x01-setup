c0aebcdf3dc6e53bce3075b393da2711ec845d11
Alle4n <allennajjuma96@gmail.com>
2025-07-20 05:40:06 +0300

    Set up navigation between pages

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+
+const PostsPage = () => {
+  return (
+    <>
+      <Header />
+      <div className="p-4">Posts Page</div>
+    </>
+  );
+};
+
+export default PostsPage;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,12 @@
+import Header from "@/components/layout/Header";
+
+const UsersPage = () => {
+  return (
+    <>
+      <Header />
+      <div className="p-4">Users Page</div>
+    </>
+  );
+};
+
+export default UsersPage;
