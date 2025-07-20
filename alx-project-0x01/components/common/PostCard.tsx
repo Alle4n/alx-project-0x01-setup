1edd1326a038d5c70a5cfa3c2e3b101083891e02
Alle4n <allennajjuma96@gmail.com>
2025-07-20 16:37:13 +0300

    Set up navigation between pages

---
+++ alx-project-0x01/components/common/PostCard.tsx
@@ -0,0 +1,5 @@
+const PostCard = () => {
+  return <div className="border p-4 shadow">Post content here</div>;
+};
+
+export default PostCard;
---
+++ alx-project-0x01/components/layout/Footer.tsx
@@ -0,0 +1,5 @@
+const Footer = () => {
+  return <footer className="p-4 bg-gray-200">Footer</footer>;
+};
+
+export default Footer;
